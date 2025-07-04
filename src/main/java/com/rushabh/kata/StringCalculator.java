137e4b05b409170e4f44097f0f57fcd1df457c8d
Rushabh <rushabhbarot812@gmail.com>
2025-07-05 01:43:30 +0530

    GREEN: Implemented add method to handle empty string and return 0

---
+++ src/main/java/com/rushabh/kata/StringCalculator.java
@@ -0,0 +1,14 @@
+package com.rushabh.kata;
+
+public class StringCalculator {
+
+    /**
+     * Takes a string of numbers and returns their sum.
+     * @param numbers The input string of numbers.
+     * @return The sum as an integer.
+     */
+
+    public int add(String numbers) {
+        return 0; // The simplest code to make the first test pass
+    }
+}
