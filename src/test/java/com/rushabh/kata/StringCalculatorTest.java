766e630eec9ae5b910cb363b6e7c244d7c68f70c
Rushabh <rushabhbarot812@gmail.com>
2025-07-05 01:55:40 +0530

    RED: Added failing test for handling a single number

--- src/test/java/com/rushabh/kata/StringCalculatorTest.java
+++ src/test/java/com/rushabh/kata/StringCalculatorTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
+
     @Test
     @DisplayName("Should return 0 for an empty string")
     void testAdd_shouldReturnZero_forEmptyString() {
@@ -13,6 +14,21 @@ public class StringCalculatorTest {
         StringCalculator calculator = new StringCalculator();
         String input = "";
         int expected = 0;
+
+        // Act
+        int result = calculator.add(input);
+
+        // Assert
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("Should return the number itself for a single number string")
+    void testAdd_shouldReturnNumber_forSingleNumber() {
+        // Arrange
+        StringCalculator calculator = new StringCalculator();
+        String input = "1";
+        int expected = 1;
 
         // Act
         int result = calculator.add(input);
